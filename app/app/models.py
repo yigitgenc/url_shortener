816818a93b7aa04ea96c4d1109369b692fc510a8
Yigit Genc <yigidix@gmail.com>
2019-05-09 03:49:01 +0200

    Create models module

---
+++ app/app/models.py
@@ -0,0 +1,28 @@
+"""Application models module."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Url(db.Model):
+    """
+    Url model.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), nullable=False, unique=True)
+    code = db.Column(db.String(8), nullable=False, unique=True, index=True)
+    visits = db.relationship("Visit", back_populates="url")
+    created = db.Column(db.DateTime, server_default=db.func.now())
+
+
+class Visit(db.Model):
+    """
+    Visit model.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    url_id = db.Column(db.Integer, db.ForeignKey(Url.id), nullable=False, index=True)
+    url = db.relationship("Url", back_populates="visits")
+    ip_address = db.Column(db.String(50), nullable=False)
+    user_agent = db.Column(db.String(255), nullable=False)
+    created = db.Column(db.DateTime, server_default=db.func.now())
